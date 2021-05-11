95040eb418bdb7b944daff6145ec91408e4c9c1e
quack <kubaice3@gmail.com>
2021-05-11 09:34:16 +0200

    Created activities list using ListView.separated()

--- lib/ManagerPage.dart
+++ lib/ManagerPage.dart
@@ -8,6 +8,22 @@ class ManagerPage extends StatefulWidget {
 
 class _ManagerPageState extends State<ManagerPage> {
 
+  List<Icon> _leading = [
+    Icon(Icons.edit_outlined, size: 20),
+    Icon(Icons.gamepad_outlined, size: 20),
+    Icon(Icons.school_outlined, size: 20),
+    Icon(Icons.work_outline, size: 20),
+    Icon(Icons.run_circle_outlined, size: 20)];
+  List<Text> _title = [
+    Text("Drawing", style: TextStyle(color: Color(0xff3A3A3A))),
+    Text("Gaming", style: TextStyle(color: Color(0xff3A3A3A))),
+    Text("School", style: TextStyle(color: Color(0xff3A3A3A))),
+    Text("School", style: TextStyle(color: Color(0xff3A3A3A))),
+    Text("Work", style: TextStyle(color: Color(0xff3A3A3A))),
+    Text("Running", style: TextStyle(color: Color(0xff3A3A3A)))];
+  Icon _trailing = Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb));
+  Color _color = Color(0xFFFDFDFB);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,47 +38,52 @@ class _ManagerPageState extends State<ManagerPage> {
         )
       ),
       body: Center(
-        child: ListView(
+        child: ListView.separated(
+          itemCount: _leading.length,
           padding: EdgeInsets.only(top:40),
-          children: ListTile.divideTiles(
-            context: context,
-            tiles: [
-              ListTile(
-                /*focusColor: ,*/
-                leading: Icon(Icons.edit_outlined, size: 20),
-                title: Text("Drawing", style: TextStyle(color: Color(0xff3A3A3A))),
-                trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
-                tileColor: Color(0xFFFDFDFB),
-              ),
-              ListTile(
-                leading: Icon(Icons.gamepad_outlined, size: 20),
-                title: Text("Gaming"),
-                trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
-                tileColor: Color(0xFFFDFDFB),
-              ),
-              ListTile(
-                leading: Icon(Icons.school_outlined, size: 20),
-                title: Text("School"),
-                trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
-                tileColor: Color(0xFFFDFDFB),
-              ),
-              ListTile(
-                leading: Icon(Icons.work_outline, size: 20),
-                title: Text("Work"),
-                trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
-                tileColor: Color(0xFFFDFDFB),
-              ),
-              ListTile(
-                leading: Icon(Icons.run_circle_outlined, size: 20),
-                title: Text("Running"),
-                trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
-                tileColor: Color(0xFFFDFDFB),
-              )
-            ]
-          ).toList()
+          separatorBuilder: (context, int index) => Divider(),
+          itemBuilder: (BuildContext contetx, int index) =>
+            ListTile(
+              leading: _leading[index],
+              title: _title[index],
+              trailing: _trailing,
+              tileColor: _color,
+            ),
         ),
         //todo list item adding button
       ),
     );
   }
 }
+
+/*
+ListTile(
+leading: Icon(Icons.edit_outlined, size: 20),
+title: Text("Drawing", style: TextStyle(color: Color(0xff3A3A3A))),
+trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
+tileColor: Color(0xFFFDFDFB),
+),
+ListTile(
+leading: Icon(Icons.gamepad_outlined, size: 20),
+title: Text("Gaming"),
+trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
+tileColor: Color(0xFFFDFDFB),
+),
+ListTile(
+leading: Icon(Icons.school_outlined, size: 20),
+title: Text("School"),
+trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
+tileColor: Color(0xFFFDFDFB),
+);
+ListTile(
+leading: Icon(Icons.work_outline, size: 20),
+title: Text("Work"),
+trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
+tileColor: Color(0xFFFDFDFB),
+);
+ListTile(
+leading: Icon(Icons.run_circle_outlined, size: 20),
+title: Text("Running"),
+trailing: Icon(Icons.arrow_forward_ios, size: 15, color: Color(0xffdbdbdb)),
+tileColor: Color(0xFFFDFDFB),
+);*/
