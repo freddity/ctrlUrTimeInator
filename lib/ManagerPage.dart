3f0c8fabc60f6a51c70f208e8b2034236962687e
quack <kubaice3@gmail.com>
2021-05-12 23:42:28 +0200

    Started working on slivers and list and cards; it started looking like ios app

--- lib/ManagerPage.dart
+++ lib/ManagerPage.dart
@@ -42,60 +42,59 @@ class _ManagerPageState extends State<ManagerPage> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoSliverNavigationBar(),
-      child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Text('Personal Info'),
-            Text('t1'),
-          ],
-        ),
-      ),
-      backgroundColor: Color(0xffEFEEF3),
-      child: Column(children: [
-        Container(
-            padding: EdgeInsets.only(left: 27, bottom: 7),
-            alignment: Alignment.centerLeft,
-            child: Text('ACTIVITIES (${_leading.length})',
-                style: TextStyle(color: Color(0xff8C8C8A), fontSize: 11.5))),
-        Expanded(
-          child: ListView.builder(
-              physics: const AlwaysScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              itemCount: 15,
-              itemBuilder: (context, index) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(
+            trailing: Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Icon(Icons.edit_outlined,
+                  color: Color.fromRGBO(0, 122, 255, 1.0)),
+            ),
+            largeTitle: Text("Manager",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff3A3A3A),
+                    fontSize: 34,
+                    letterSpacing: 0.1)),
+          ),
+          SliverPadding(padding: EdgeInsets.only(top: 20)),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
                 return Card(
-                  margin: EdgeInsets.only(bottom: 15.0, left: 10, right: 10),
+                  margin: EdgeInsets.only(bottom: 10.0, left: 10, right: 10),
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15.0)),
                   child:
                       Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Color(0xffffffff),
-                              borderRadius: BorderRadius.circular(15.0),
-                              boxShadow: [
-                                BoxShadow(
-                                    blurRadius: 8,
-                                    offset: Offset(0, 15),
-                                    color: Color(0xffE9E8EB).withOpacity(0.6),
-                                    spreadRadius: -9)
-                              ]),
-
-                          child: ListTile(
-                            leading: const Icon(Icons.album),
-                            title: Text('The  is having:'),
-                            subtitle: Text(' Votes.'),
-                            shape: RoundedRectangleBorder(
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xffffffff),
+                          borderRadius: BorderRadius.circular(15.0),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 8,
+                                offset: Offset(0, 0),
+                                color: Color(0xffE9E8EB).withOpacity(0.3),
+                                spreadRadius: -9)
+                          ]),
+                      child: ListTile(
+                        leading: const Icon(Icons.album),
+                        title: Text('The  is having:'),
+                        subtitle: Text(' Votes.'),
+                        shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15.0)),
+                      ),
                     ),
-                        ),
                   ]),
                 );
-              }),
-        )
-      ]),
+              },
+              childCount: 20,
+            ),
+          )
+        ],
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Container(
         width: 40,
