cfbc054acd8d83e7040d989e7036c911f37c9e0b
quack <kubaice3@gmail.com>
2021-05-27 10:29:39 +0200

    The two last things to do with charts are design selecting and add percentage value to the labels

--- lib/StatisticsOneDay.dart
+++ lib/StatisticsOneDay.dart
@@ -13,44 +13,55 @@ class _StatisticsOneDay extends State<StatisticsOneDay> {
       body: SfCircularChart(
         margin: EdgeInsets.zero,
         centerY: '120',
-        series: <CircularSeries>[
-          PieSeries<GDPData, String>(
+        series: <DoughnutSeries>[
+          DoughnutSeries<ChartData, String>(
             dataSource: getChartData(),
-            xValueMapper: (GDPData data, _) => data.continent,
-            yValueMapper: (GDPData data, _) => data.gdp,
-            dataLabelMapper: (GDPData data, _) => data.continent,
+            xValueMapper: (ChartData data, _) => data.title,
+            yValueMapper: (ChartData data, _) => data.value,
+            pointColorMapper: (ChartData data, _) => data.color,
+            dataLabelMapper: (ChartData data, _) => data.title,
             dataLabelSettings: DataLabelSettings(
+              textStyle: TextStyle(color: Color.fromRGBO(44, 44, 46, 1), fontSize: 10),
               isVisible: true,
               labelPosition: ChartDataLabelPosition.outside,
               showCumulativeValues: true,
+              connectorLineSettings:
+              ConnectorLineSettings(width: 0.5, color: Color(0x8a7f7f7f)),
             ),
             enableTooltip: true,
             enableSmartLabels: true,
             explode: true,
             animationDuration: 350,
             radius: '60%',
+            selectionBehavior: SelectionBehavior(
+                enable: true,
+                unselectedOpacity: 0.3,
+                unselectedBorderWidth: 0.1,
+                unselectedBorderColor: Colors.black
+            ),
           )
         ],
       ),
     );
   }
-
-  List<GDPData> getChartData() {
-    final List<GDPData> chartData = [
-      GDPData('Oceania', 5),
-      GDPData('Africa', 85),
-      GDPData('S America', 50),
-      GDPData('Europe', 8),
-      GDPData('N America', 27),
-      GDPData('Asia', 30)
-    ];
-    return chartData;
-  }
 }
 
-class GDPData {
-  GDPData(this.continent, this.gdp);
+List<ChartData> getChartData() {
+  final List<ChartData> chartData = [
+    ChartData('Drawing', 5, Color.fromRGBO(49, 152, 252, 1.0)),
+    ChartData('Gaming', 85, Color.fromRGBO(58, 206, 94, 1.0)),
+    ChartData('School', 50, Color.fromRGBO(117, 115, 231, 1.0)),
+    ChartData('Work', 8, Color.fromRGBO(250, 173, 57, 1.0)),
+    ChartData('Running', 27, Color.fromRGBO(248, 88, 120, 1.0)),
+    ChartData('Learning Norwegian', 30, Color.fromRGBO(196, 111, 238, 1.0))
+  ];
+  return chartData;
+}
 
-  final String continent;
-  final int gdp;
+class ChartData {
+  ChartData(this.title, this.value, this.color);
+
+  final String title;
+  final int value;
+  final Color color;
 }
--- lib/StatisticsSevenDay.dart
+++ lib/StatisticsSevenDay.dart
@@ -13,44 +13,56 @@ class _StatisticsSevenDay extends State<StatisticsSevenDay> {
       body: SfCircularChart(
         margin: EdgeInsets.zero,
         centerY: '120',
-        series: <CircularSeries>[
-          PieSeries<ChartData, String>(
+        series: <DoughnutSeries>[
+          DoughnutSeries<ChartData, String>(
             dataSource: getChartData(),
             xValueMapper: (ChartData data, _) => data.title,
             yValueMapper: (ChartData data, _) => data.value,
+            pointColorMapper: (ChartData data, _) => data.color,
             dataLabelMapper: (ChartData data, _) => data.title,
             dataLabelSettings: DataLabelSettings(
+              textStyle: TextStyle(
+                  color: Color.fromRGBO(44, 44, 46, 1), fontSize: 10),
               isVisible: true,
               labelPosition: ChartDataLabelPosition.outside,
               showCumulativeValues: true,
+              connectorLineSettings:
+              ConnectorLineSettings(width: 0.5, color: Color(0x8a7f7f7f)),
             ),
             enableTooltip: true,
             enableSmartLabels: true,
             explode: true,
             animationDuration: 350,
             radius: '60%',
+            selectionBehavior: SelectionBehavior(
+                enable: true,
+                unselectedOpacity: 0.3,
+                unselectedBorderWidth: 0.1,
+                unselectedBorderColor: Colors.black
+            ),
           )
         ],
       ),
     );
   }
+}
 
-  List<ChartData> getChartData() {
-    final List<ChartData> chartData = [
-      ChartData('Oceania', 5),
-      ChartData('Africa', 200),
-      ChartData('S America', 500),
-      ChartData('Europe', 40),
-      ChartData('N America', 50),
-      ChartData('Asia', 70)
-    ];
-    return chartData;
-  }
+List<ChartData> getChartData() {
+  final List<ChartData> chartData = [
+    ChartData('Drawing', 5, Color.fromRGBO(49, 152, 252, 1.0)),
+    ChartData('Gaming', 200, Color.fromRGBO(58, 206, 94, 1.0)),
+    ChartData('School', 500, Color.fromRGBO(117, 115, 231, 1.0)),
+    ChartData('Work', 40, Color.fromRGBO(250, 173, 57, 1.0)),
+    ChartData('Running', 50, Color.fromRGBO(248, 88, 120, 1.0)),
+    ChartData('Learning Norwegian', 70, Color.fromRGBO(196, 111, 238, 1.0))
+  ];
+  return chartData;
 }
 
 class ChartData {
-  ChartData(this.title, this.value);
+  ChartData(this.title, this.value, this.color);
 
   final String title;
   final int value;
+  final Color color;
 }
